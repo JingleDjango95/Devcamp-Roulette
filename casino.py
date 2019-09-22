a49ed2748bcfcf8ae4ff2df02b9e6d4f8363b4cc
JingleDjango95 <spencerweber019@gmail.com>
2019-09-22 08:48:37 -0600

    Initial code

---
+++ casino.py
@@ -0,0 +1,20 @@
+# We are building a casino!
+# You are to build one casino game, black jack, slots, roulette, etc, ( these are the ones I have seen built out before.)
+# requirements
+# Build the game.
+# Push individual commits to github.
+# In addition,
+# the player will have a wallet, they can add money to the wallet, any winnings they get will be added to the wallet.
+# If the wallet goes empty they must add more money or they have to stop playing.
+# They need to be able to stop playing and cash out the wallet at anytime. (bonus if the cash out proccess tells them if they made a profit or not)
+# let the player set the dollar/cents amount of the bet (in the game).
+# let them continue playing as long as they have money or decide to cash out.
+
+# 0 and 00 are green
+# 1 - 10 evens are black, odds are red
+# 11 - 18 evens are red, odds are black
+# 19 - 28 evens are black, odds are red
+# 29 - 36 evens are red, odds are black
+
+# rules
+
