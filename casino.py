78c19eaa81bf9c48ab85a5f5181ca6fd2081bbbd
JingleDjango95 <spencerweber019@gmail.com>
2019-09-24 09:17:41 -0600

    Rearranged the buttons and started work on the bottom menu

--- casino.py
+++ casino.py
@@ -52,55 +52,68 @@ def roulette(number):
     master = Tk()
     master.title('Devcamp Roulette')
 
-    topFrame = Frame(master, width=1080, height=540)
-    topFrame.pack()
-    bottomFrame = Frame(master, width=1080, height=540)
-    bottomFrame.pack(side=BOTTOM)
+    topFrame = Frame(master, width=500, height=200)
+    topFrame.pack(anchor=N)
 
-    button_0 = Button(topFrame, text="0", fg='green').grid(row=0)
-    button_00 = Button(topFrame, text="00", fg='green').grid(row=2)
-    button_1 = Button(topFrame, text="1", fg='red').grid(row=0, column=1)
-    button_2 = Button(topFrame, text="2").grid(row=1, column=1)
-    button_3 = Button(topFrame, text="3", fg='red').grid(row=2, column=1)
-    button_4 = Button(topFrame, text="4").grid(row=0, column=2)
-    button_5 = Button(topFrame, text="5", fg='red').grid(row=1, column=2)
-    button_6 = Button(topFrame, text="6").grid(row=2, column=2)
-    button_7 = Button(topFrame, text="7", fg='red').grid(row=0, column=3)
-    button_8 = Button(topFrame, text="8").grid(row=1, column=3)
-    button_9 = Button(topFrame, text="9", fg='red').grid(row=2, column=3)
-    button_10 = Button(topFrame, text="10").grid(row=0, column=4)
+    button_0 = Button(topFrame, text="0", fg='green', width=2).grid(row=0)
+    button_00 = Button(topFrame, text="00", fg='green', width=2).grid(row=2)
+
+    button_3 = Button(topFrame, text="3", fg='red', width=2).grid(row=0, column=1)
+    button_2 = Button(topFrame, text="2", width=2).grid(row=1, column=1)
+    button_1 = Button(topFrame, text="1", fg='red', width=2).grid(row=2, column=1)
+
+    button_6 = Button(topFrame, text="6", width=2).grid(row=0, column=2)
+    button_5 = Button(topFrame, text="5", fg='red', width=2).grid(row=1, column=2)
+    button_4 = Button(topFrame, text="4", width=2).grid(row=2, column=2)
+
+    button_9 = Button(topFrame, text="9", fg='red', width=2).grid(row=0, column=3)
+    button_8 = Button(topFrame, text="8", width=2).grid(row=1, column=3)
+    button_7 = Button(topFrame, text="7", fg='red', width=2).grid(row=2, column=3)
+
+    button_12 = Button(topFrame, text="12", fg='red').grid(row=0, column=4)
     button_11 = Button(topFrame, text="11").grid(row=1, column=4)
-    button_12 = Button(topFrame, text="12", fg='red').grid(row=2, column=4)
-    button_13 = Button(topFrame, text="13").grid(row=0, column=5)
+    button_10 = Button(topFrame, text="10").grid(row=2, column=4)
+
+    button_15 = Button(topFrame, text="15").grid(row=0, column=5)
     button_14 = Button(topFrame, text="14", fg='red').grid(row=1, column=5)
-    button_15 = Button(topFrame, text="15").grid(row=2, column=5)
-    button_16 = Button(topFrame, text="16", fg='red').grid(row=0, column=6)
+    button_13 = Button(topFrame, text="13").grid(row=2, column=5)
+
+    button_18 = Button(topFrame, text="18", fg='red').grid(row=0, column=6)
     button_17 = Button(topFrame, text="17").grid(row=1, column=6)
-    button_18 = Button(topFrame, text="18", fg='red').grid(row=2, column=6)
-    button_19 = Button(topFrame, text="19", fg='red').grid(row=0, column=7)
+    button_16 = Button(topFrame, text="16", fg='red').grid(row=2, column=6)
+
+    button_21 = Button(topFrame, text="21", fg='red').grid(row=0, column=7)
     button_20 = Button(topFrame, text="20").grid(row=1, column=7)
-    button_21 = Button(topFrame, text="21", fg='red').grid(row=2, column=7)
-    button_22 = Button(topFrame, text="22").grid(row=0, column=8)
+    button_19 = Button(topFrame, text="19", fg='red').grid(row=2, column=7)
+
+    button_24 = Button(topFrame, text="24").grid(row=0, column=8)
     button_23 = Button(topFrame, text="23", fg='red').grid(row=1, column=8)
-    button_24 = Button(topFrame, text="24").grid(row=2, column=8)
-    button_25 = Button(topFrame, text="25", fg='red').grid(row=0, column=9)
+    button_22 = Button(topFrame, text="22").grid(row=2, column=8)
+
+    button_27 = Button(topFrame, text="27", fg='red').grid(row=0, column=9)
     button_26 = Button(topFrame, text="26").grid(row=1, column=9)
-    button_27 = Button(topFrame, text="27", fg='red').grid(row=2, column=9)
-    button_28 = Button(topFrame, text="28").grid(row=0, column=10)
+    button_25 = Button(topFrame, text="25", fg='red').grid(row=2, column=9)
+
+    button_30 = Button(topFrame, text="30", fg='red').grid(row=0, column=10)
     button_29 = Button(topFrame, text="29").grid(row=1, column=10)
-    button_30 = Button(topFrame, text="30", fg='red').grid(row=2, column=10)
-    button_31 = Button(topFrame, text="31").grid(row=0, column=11)
+    button_28 = Button(topFrame, text="28").grid(row=2, column=10)
+
+    button_33 = Button(topFrame, text="33").grid(row=0, column=11)
     button_32 = Button(topFrame, text="32", fg='red').grid(row=1, column=11)
-    button_33 = Button(topFrame, text="33").grid(row=2, column=11)
-    button_34 = Button(topFrame, text="34", fg='red').grid(row=0, column=12)
+    button_31 = Button(topFrame, text="31").grid(row=2, column=11)
+
+    button_36 = Button(topFrame, text="36", fg='red').grid(row=0, column=12)
     button_35 = Button(topFrame, text="35").grid(row=1, column=12)
-    button_36 = Button(topFrame, text="36", fg='red').grid(row=2, column=12)
+    button_34 = Button(topFrame, text="34", fg='red').grid(row=2, column=12)
+
     button_c1 = Button(topFrame, text='2 to 1').grid(row=0, column=13)
     button_c2 = Button(topFrame, text='2 to 1').grid(row=1, column=13)
     button_c3 = Button(topFrame, text='2 to 1').grid(row=2, column=13)
+
     button_first12 = Button(topFrame, text='1st 12', width=12).grid(row=3, column=1, columnspan=4)
     button_second12 = Button(topFrame, text='2nd 12', width=12).grid(row=3, column=5, columnspan=4)
     button_third12 = Button(topFrame, text='3rd 12', width=12).grid(row=3, column=9, columnspan=4)
+
     button_first18 = Button(topFrame, text='1 to 18', width=6).grid(row=4, column=1, columnspan=2)
     button_even = Button(topFrame, text='EVEN', width=6).grid(row=4, column=3, columnspan=2)
     button_red = Button(topFrame, text='RED', width=6, fg='red').grid(row=4, column=5, columnspan=2)
@@ -108,7 +121,21 @@ def roulette(number):
     button_odd = Button(topFrame, text='ODD', width=6).grid(row=4, column=9, columnspan=2)
     button_last18 = Button(topFrame, text='19 to 36', width=6).grid(row=4, column=11, columnspan=2)
 
-    chip_value = number // 20
+    border = Canvas(master, width=500, height=10)
+    border.pack(anchor=CENTER)
+
+    borderLine = border.create_line(0, 10, 500, 10)
+
+    bottomFrame = Frame(master, width=500, height=190)
+    bottomFrame.pack(anchor=S)
+
+    chip_count = 20
+    chip_value = number // chip_count
+
+    chip = Label(bottomFrame, text=f"You have {chip_count} chips, valued at ${chip_value:0.2f}")
+    chip.pack()
+
+    tkvar = StringVar(master)
 
     master.mainloop()
 
@@ -164,46 +191,6 @@ buy_in_button100.bind('<Button-1>', initiate_roulette_with_100)
 buy_in_button1000.bind('<Button-1>', initiate_roulette_with_1000)
 
 buy_in.mainloop()
-# import re
-
-# def start():
-#     print(' --------------')
-#     print('/   0   |  00  \\')
-#     print('----------------')
-#     print('| 1  | 2  | 3  |')
-#     print('----------------')
-#     print('| 4  | 5  | 6  |')
-#     print('----------------')
-#     print('| 7  | 8  | 9  |')
-#     print('----------------')
-#     print('| 10 | 11 | 12 |')
-#     print('----------------')
-#     print('| 13 | 14 | 15 |')
-#     print('----------------')
-#     print('| 16 | 17 | 18 |')
-#     print('----------------')
-#     print('| 19 | 20 | 21 |')
-#     print('----------------')
-#     print('| 22 | 23 | 24 |')
-#     print('----------------')
-#     print('| 25 | 26 | 27 |')
-#     print('----------------')
-#     print('| 28 | 29 | 30 |')
-#     print('----------------')
-#     print('| 31 | 32 | 33 |')
-#     print('----------------')
-#     print('| 34 | 35 | 36 |')
-#     print('----------------')
-#     print("\nWelcome to Roulette!, the rules are simple, guess the number \
-# and you win! There are several bets you can make, there are \
-# straight bets, which is one number, split bets, which is between \
-# two adjacent numbers, street bets, which is between three adjacent \
-# numbers, corner bets, which are between four adjacent numbers, \
-# the lucky five-number bet, which is only 00, 0, 1, 2, and 3, a six \
-# number bet, which is any row of six numbers, and if you don't want to \
-# risk it all, you can make safer bets with black, red, even odd, the numbers \
-# 1-18, or 19-36, which dozen will win, and which column will win.\n")
-#     buy_in()
 
 # def buy_in():
 #     # player inputs buy in amount and is given the chips for it
